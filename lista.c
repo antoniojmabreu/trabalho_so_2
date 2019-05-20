6d5ebddf0497ae76d13e9a5e4abd41152aca6444
antoniojmabreu <antoniojmabreu@gmail.com>
2019-05-20 14:29:31 +0100

    comentários das funcs

--- lista.c
+++ lista.c
@@ -5,15 +5,16 @@
 #define SIZE 40
 
 //typedef's
-typedef struct ABSTRATA {  //pixeis pertencentes ao intervalo de cor num determinado frame
+typedef struct ABSTRATA {  //struc para guardar as mensagens com os respetivos IDs
   int msgId;
   char content[SIZE];
   struct ABSTRATA *nseg;
 }Abstrata;
 
 
-Abstrata* insertNode(Abstrata *A, Abstrata *nv) {  //estilo insertFirst para não ter de correr as listas para descobrir o ultimo elemento
-                                          //mais rápido do que a altenativa insertLast
+Abstrata* insertNode(Abstrata *A, Abstrata *nv) {     //insere uma nova mensagem na struct
+                                                      //estilo insertFirst para não ter de correr as listas para descobrir o ultimo elemento
+                                                      //mais rápido do que a altenativa insertLast
 	if(A == NULL)
 		return nv;
 
@@ -41,7 +42,7 @@ Abstrata* removeNodeId(Abstrata *A, int id) {  //remove por id e devolve cabeça
   return head;
 }
 
-Abstrata* createNode(int ID) {
+Abstrata* createNode(int ID) {  //cria uma nova mensagem
   char text[SIZE];
 
   printf("Insert content\n");
@@ -54,7 +55,7 @@ Abstrata* createNode(int ID) {
   return newAbstrata;
 }
 
-void listNodes(Abstrata  *A) {
+void listNodes(Abstrata  *A) {  //lista todas as mensagens
   while(A != NULL) {
     printf("\nmsgId: %d\n", A->msgId);
     printf("content: \n%s\n\n", A->content);
@@ -67,7 +68,7 @@ void printMenu() {
   printf("\n\n1 -> Insert new\n2 -> List all\n3 -> Remove msg\n0 -> exit\n\n->");
 }
 
-int main() {
+int main() {  //main
   Abstrata *list = NULL;
   int ID = 0, select, x;
 
@@ -77,7 +78,7 @@ int main() {
     scanf("%d", &select);
     switch (select) {
       case 1:
-        //inserir novo
+        //inserir nova mensagem na lista
         ID++;
         Abstrata *new = createNode(ID);
         list = insertNode(list, new);
@@ -86,7 +87,7 @@ int main() {
       break;
 
       case 2:
-        //listar todos
+        //listar todas as mensagens
         if(list == NULL)
           printf("\nNo messages\n");
 
@@ -97,7 +98,7 @@ int main() {
       break;
 
       case 3:
-        //remove por id
+        //remove mensagem por id
         printf("Insert id to remove\n");
         scanf("%d", &x);
 
