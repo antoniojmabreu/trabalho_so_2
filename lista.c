6b0d297545301c5516394a4e28cc99ebb18c9dbb
antoniojmabreu <antoniojmabreu@gmail.com>
2019-05-20 09:32:04 +0100

    funções insert e remove nodes

--- lista.c
+++ lista.c
@@ -8,3 +8,32 @@ typedef struct ABSTRATA {  //pixeis pertencentes ao intervalo de cor num determi
   char[SIZE]
   struct ABSTRATA *nseg;
 }Abstrata;
+
+Abstrata* insertNode(Abstrata *A, Abstrata *nv) {  //estilo insertFirst para não ter de correr as listas para descobrir o ultimo elemento
+                                          //mais rápido do que a altenativa insertLast
+	if(A == NULL)
+		return nv;
+
+  nv->nseg = A;
+  return nv;
+}
+
+Abstrata* removeAbstrata(Abstrata *A, int id) {  //remove por id e devolve cabeça da lista atualizada
+  Abstrata *head = A, *prev = A;
+
+  while(A != NULL) {
+    if(A->msgId == id) {
+      if(A == prev) {
+        A = A->nseg;
+        prev->nseg = NULL;
+        return A;
+      }
+      prev->nseg = A->nseg;
+      A->nseg = NULL;
+      return head;
+    }
+    prev = A;
+    A = A->nseg;
+  }
+  return head;
+}
