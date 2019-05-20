58c8ef9d3cb13f8a15baaddc2d83615f164490b3
antoniojmabreu <antoniojmabreu@gmail.com>
2019-05-20 09:25:41 +0100

    lista ligada simples

---
+++ lista.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 40
+
+//typedef's
+typedef struct ABSTRATA {  //pixeis pertencentes ao intervalo de cor num determinado frame
+  int msgId;
+  char[SIZE]
+  struct ABSTRATA *nseg;
+}Abstrata;
