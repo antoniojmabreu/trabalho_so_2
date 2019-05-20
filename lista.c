1bb8c594531b9d3c4403ad6fe0f80bee03982a9b
antoniojmabreu <antoniojmabreu@gmail.com>
2019-05-20 10:22:12 +0100

    funções de manipulação de listas DONE

--- lista.c
+++ lista.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <string.h>
+
 #define SIZE 40
 
 //typedef's
 typedef struct ABSTRATA {  //pixeis pertencentes ao intervalo de cor num determinado frame
   int msgId;
-  char[SIZE]
+  char content[SIZE];
   struct ABSTRATA *nseg;
 }Abstrata;
+
 
 Abstrata* insertNode(Abstrata *A, Abstrata *nv) {  //estilo insertFirst para não ter de correr as listas para descobrir o ultimo elemento
                                           //mais rápido do que a altenativa insertLast
@@ -36,4 +39,82 @@ Abstrata* removeNodeId(Abstrata *A, int id) {  //remove por id e devolve cabeça
     A = A->nseg;
   }
   return head;
+}
+
+Abstrata* createNode(int ID) {
+  char text[SIZE];
+
+  printf("Insert content\n");
+  scanf("%s", text);
+
+  Abstrata *newAbstrata = (Abstrata*)malloc(sizeof(Abstrata));
+  newAbstrata->msgId = ID;
+  strcpy(newAbstrata->content, text);
+
+  return newAbstrata;
+}
+
+void listNodes(Abstrata  *A) {
+  while(A != NULL) {
+    printf("\nmsgId: %d\n", A->msgId);
+    printf("content: \n%s\n\n", A->content);
+
+    A = A->nseg;
+  }
+}
+
+void printMenu() {
+  printf("\n\n1 -> Insert new\n2 -> List all\n3 -> Remove msg\n0 -> exit\n\n->");
+}
+
+int main() {
+  Abstrata *list = NULL;
+  int ID = 0, select, x;
+
+  printMenu();
+
+  while(1) {
+    scanf("%d", &select);
+    switch (select) {
+      case 1:
+        //inserir novo
+        ID++;
+        Abstrata *new = createNode(ID);
+        list = insertNode(list, new);
+
+        printMenu();
+      break;
+
+      case 2:
+        //listar todos
+        if(list == NULL)
+          printf("\nNo messages\n");
+
+        else
+          listNodes(list);
+
+        printMenu();
+      break;
+
+      case 3:
+        //remove por id
+        printf("Insert id to remove\n");
+        scanf("%d", &x);
+
+        list = removeNodeId(list, x);
+
+        printMenu();
+      break;
+
+      case 0:
+        //sair
+        exit(1);
+      break;
+
+      default:
+        printf("\n !!! Seleção inválida !!!\n\n Selecione Opção -> ");
+        continue;
+      break;
+    }
+  }
 }
