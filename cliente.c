187b072e286aa5c8a1ad6cea6870f64807d7dfe5
antoniojmabreu <antoniojmabreu@gmail.com>
2019-06-02 16:41:54 +0100

    .

---
+++ cliente.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <pthread.h>
+#define SR sizeof(float)
+#define FIFO1 "/tmp/fifo.1"
+#define FIFO2 "/tmp/fifo.2"
+#define PERMS 0666
+#define SIZE 500
+
+//typedef's
+typedef struct ABSTRATA {  //struc para guardar as mensagens com os respetivos IDs
+  int msgId;
+  char content[SIZE];
+  struct ABSTRATA *nseg;
+}Abstrata;
+
+typedef struct SELECT_T {
+  int select;
+}select_t;
+
+void listNodes(Abstrata  *A) {  //lista todas as mensagens
+  while(A != NULL) {
+    printf("\nmsgId: %d\n", A->msgId);
+    printf("content: \n%s\n\n", A->content);
+
+    A = A->nseg;
+  }
+}
+
+void printMenu() {
+  printf("\n\n1 -> New message\n2 -> Show all messages\n3 -> Remove message\n0 -> exit\n\n->");
+}
+
+//void * thread(void *args) {
+int main() {
+
+  mknod(FIFO1, S_IFIFO | PERMS, 0);
+  mknod(FIFO2, S_IFIFO | PERMS, 0);
+  float readfd, writefd;
+  int ID = 0, x, msgid, flag, select;
+  char content[SIZE] = {}, msgcnt[SIZE];
+  //select_t * ptr = (select_t *)args;
+  printf("check 2\n" );
+  printMenu();
+
+  while(1) {
+    scanf("%d", &select);
+    switch (select) {
+      case 1:
+        //inserir nova mensagem na lista
+        ID++;
+
+        printf("Type mesage content:\n");
+        //fgets(content, SIZE, stdin);
+        scanf("%s", content);
+        printf("%s\n", content);
+
+        writefd = open(FIFO1, 1);
+        write(writefd, &select, sizeof(int));
+        write(writefd, &ID, sizeof(int));
+        write(writefd, &x, sizeof(int));
+        write(writefd, &content, sizeof(content));
+
+        printMenu();
+      break;
+
+      case 2:
+        //listar todas as mensagens
+        writefd = open(FIFO1, 1);
+        write(writefd, &select, sizeof(int));
+        write(writefd, &ID, sizeof(int));
+        write(writefd, &x, sizeof(int));
+        write(writefd, &content, sizeof(content));
+
+        while (1) {
+          readfd = open(FIFO2, 0);
+          read(readfd, &msgid, sizeof(int));
+          read(readfd, &msgcnt, sizeof(msgcnt));
+          read(readfd, &flag, sizeof(int));
+
+          if(flag == -1) {
+            printf("\nNo messages\n");
+            break;
+          }
+
+          printf("msgid %d\n", msgid);
+          printf("msgcnt %s\n\n", msgcnt);
+
+          if(flag == 1)
+            break;
+        }
+
+        printMenu();
+      break;
+
+      case 3:
+        //remove mensagem por id
+        printf("Insert id to remove\n");
+        scanf("%d", &x);
+
+        writefd = open(FIFO1, 1);
+        write(writefd, &select, sizeof(int));
+        write(writefd, &ID, sizeof(int));
+        write(writefd, &x, sizeof(int));
+        write(writefd, &content, sizeof(content));
+
+        printMenu();
+      break;
+
+      case 0:
+        //sair
+        exit(1);
+      break;
+
+      default:
+        printf("\n !!! Seleção inválida !!!\n\n Selecione Opção -> ");
+        continue;
+      break;
+    }
+  }
+
+  //readfd = open(FIFO2, 0);
+  //read(readfd, &res, SR);
+
+  unlink(FIFO1);
+  unlink(FIFO2);
+  //printf("Result %.3f\n",res);
+
+  return 0;
+}
+/*
+int main() {
+  int select = 0;
+  pthread_t th;
+
+  while (1) {
+    printMenu();
+    scanf("%d", &select);
+    select_t * ptr = (select_t *)malloc( sizeof(select_t));
+    ptr->select = select;
+    pthread_create(&th, NULL, thread, (void *)ptr);
+    pthread_join(th, NULL);
+    printf("check 1\n" );
+  }
+}
+*/
---
+++ servidor.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#define SR sizeof(float)
+#define FIFO1 "/tmp/fifo.1"
+#define FIFO2 "/tmp/fifo.2"
+#define PERMS 0666
+#define SIZE 500
+
+
+//typedef's
+typedef struct ABSTRATA {  //struc para guardar as mensagens com os respetivos IDs
+  int msgId;
+  char content[SIZE];
+  struct ABSTRATA *nseg;
+}Abstrata;
+
+
+Abstrata* insertNode(Abstrata *A, Abstrata *nv) {      //insere uma nova mensagem na struct
+                                                       //estilo insertFirst para não ter de correr as listas para descobrir o ultimo elemento
+	if(A == NULL)                                        //mais rápido do que a altenativa insertLast
+		return nv;
+
+  nv->nseg = A;
+  return nv;
+}
+
+Abstrata* removeNodeId(Abstrata *A, int id) {  //remove por id e devolve cabeça da lista atualizada
+  Abstrata *head = A, *prev = A;
+
+  while(A != NULL) {
+    if(A->msgId == id) {
+      if(A == prev) {
+        A = A->nseg;
+        prev->nseg = NULL;
+        return A;
+      }
+      prev->nseg = A->nseg;
+      A->nseg = NULL;
+      return head;
+    }
+    prev = A;
+    A = A->nseg;
+  }
+  return head;
+}
+
+Abstrata* createNode(int ID, char content[]) {  //cria uma nova mensagem
+
+  Abstrata *newAbstrata = (Abstrata*)malloc(sizeof(Abstrata));
+  newAbstrata->msgId = ID;
+  strcpy(newAbstrata->content, content);
+
+  return newAbstrata;
+}
+
+void listNodes(Abstrata  *A) {  //lista todas as mensagens
+  while(A != NULL) {
+    printf("\nmsgId: %d\n", A->msgId);
+    printf("content: \n%s\n\n", A->content);
+
+    A = A->nseg;
+  }
+}
+
+
+int main () {
+  mknod(FIFO1, S_IFIFO | PERMS, 0);
+  mknod(FIFO2, S_IFIFO | PERMS, 0);
+  float readfd, writefd;
+  int select, x, ID, msgid, count = 0, flag;
+  char content[SIZE], msgcnt[SIZE];
+
+  Abstrata *list = NULL, *head = NULL;
+
+  while(1) {
+    readfd = open(FIFO1, 0);
+    read(readfd, &select, sizeof(int));
+    read(readfd, &ID, sizeof(int));
+    read(readfd, &x, sizeof(int));
+    read(readfd, &content, sizeof(content));
+
+    switch (select){
+      case 1 :
+        list = insertNode(list, createNode(ID, content));
+        count++;
+      break;
+      case 2 :
+        if(list == NULL) {
+          flag = -1;
+          printf("\nNo messages\n");
+
+          writefd = open(FIFO2, 1);
+          write (writefd, &msgid, sizeof(int));
+          write (writefd, &msgcnt, sizeof(msgcnt));
+          write (writefd, &flag, sizeof(int));
+        }
+        else {
+          flag = count;
+          head = list;
+
+          while(list != NULL) {
+            msgid = list->msgId;
+            printf("%d\n", list->msgId);
+            strcpy(msgcnt, list->content);
+            writefd = open(FIFO2, 1);
+            write (writefd, &msgid, sizeof(int));
+            write (writefd, &msgcnt, sizeof(msgcnt));
+            write (writefd, &flag, sizeof(int));
+            printf("flag %d\n", flag);
+            flag--;
+
+            list = list->nseg;
+          }
+          list = head;
+          listNodes(list);
+        }
+      break;
+      case 3 :
+        list = removeNodeId(list, x);
+        listNodes(list);
+        if(count > 0)
+          count--;
+      break;
+    }
+  }
+  return 0;
+}
